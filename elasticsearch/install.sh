23341f2995c0e9e36b1c89caf4571b0e09bde3b1
ewolinetz <ewolinet@redhat.com>
2016-06-24 10:18:36 -0500

    trying to get searchguard key read to work

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -6,11 +6,16 @@ mkdir -p ${HOME}
 ln -s /usr/share/elasticsearch /usr/share/java/elasticsearch
 
 /usr/share/elasticsearch/bin/plugin install -b com.floragunn/search-guard-ssl/2.3.3.13
-/usr/share/elasticsearch/bin/plugin install -b com.floragunn/search-guard-2/2.3.3.1
+/usr/share/elasticsearch/bin/plugin install -b com.floragunn/search-guard-2/2.3.3.2
 /usr/share/elasticsearch/bin/plugin install io.fabric8/elasticsearch-cloud-kubernetes/2.3.3
 
 # TODO: update the following plugin
+#/usr/share/elasticsearch/bin/plugin install file://${HOME}/openshift-elasticsearch-plugin-2.3.3.zip
 #/usr/share/elasticsearch/bin/plugin -i io.fabric8.elasticsearch/openshift-elasticsearch-plugin/0.14
 
 mkdir /elasticsearch
+mkdir -p $ES_CONF
+chmod -R og+w $ES_CONF
 chmod -R og+w /usr/share/java/elasticsearch ${HOME} /elasticsearch
+chmod -R o+rx /etc/elasticsearch
+chmod a+x /usr/share/elasticsearch/plugins/search-guard-2/tools/sgadmin.sh
--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -2,13 +2,21 @@
 
 set -euo pipefail
 
-mkdir -p /elasticsearch/$CLUSTER_NAME
 secret_dir=/etc/elasticsearch/secret
-#[ -f $secret_dir/searchguard-node-key ] && ln -s $secret_dir/searchguard-node-key /elasticsearch/$CLUSTER_NAME/searchguard_node_key.key
-#[ -f $secret_dir/searchguard.key ] && ln -s $secret_dir/searchguard.key /elasticsearch/$CLUSTER_NAME.key
-#[ -f $secret_dir/searchguard.truststore ] && ln -s $secret_dir/searchguard.truststore /elasticsearch/$CLUSTER_NAME.truststore
-[ -f $secret_dir/keystore.password ] && export KEYSTORE_PASSWORD=$(cat $secret_dir/keystore.password)
-[ -f $secret_dir/truststore.password ] && export TRUSTSTORE_PASSWORD=$(cat $secret_dir/truststore.password)
+config_dir=/etc/elasticsearch/config
+
+# ES seems to be picky about its config/ dir and permissions within it -- ln -s would yield incorrect permissions...
+[ -f $secret_dir/searchguard.key ] && cp $secret_dir/searchguard.key $ES_CONF/$CLUSTER_NAME.key
+[ -f $secret_dir/searchguard.truststore ] && cp $secret_dir/searchguard.truststore $ES_CONF/$CLUSTER_NAME.truststore
+[ -f $config_dir/elasticsearch.yml ] && cp $config_dir/elasticsearch.yml $ES_CONF/elasticsearch.yml
+[ -f $config_dir/logging.yml ] && cp $config_dir/logging.yml $ES_CONF/logging.yml
+
+[ -f $secret_dir/key ] && cp $secret_dir/key $ES_CONF/key
+[ -f $secret_dir/truststore ] && cp $secret_dir/truststore $ES_CONF/truststore
+
+[ -f $secret_dir/admin-cert ] && cp $secret_dir/admin-cert $ES_CONF/admin-cert
+[ -f $secret_dir/admin-key ] && cp $secret_dir/admin-key $ES_CONF/admin-key
+[ -f $secret_dir/admin-ca ] && cp $secret_dir/admin-ca $ES_CONF/admin-ca
 
 # the amount of RAM allocated should be half of available instance RAM.
 # ref. https://www.elastic.co/guide/en/elasticsearch/guide/current/heap-sizing.html#_give_half_your_memory_to_lucene
@@ -29,4 +37,24 @@ else
 	exit 1
 fi
 
-exec /usr/share/elasticsearch/bin/elasticsearch
+exec /usr/share/elasticsearch/bin/elasticsearch --path.conf=$ES_CONF &
+
+# somehow wait for ES to start and then run the following?
+# ALT - make this a post deploy pod?
+
+#ln -s /usr/share/elasticsearch/config/${CLUSTER_NAME}.key /usr/share/elasticsearch/config/${CLUSTER_NAME}.key.jks
+ln -s /usr/share/elasticsearch/config/${CLUSTER_NAME}.truststore /usr/share/elasticsearch/config/${CLUSTER_NAME}.truststore.jks
+cp /etc/elasticsearch/secret/admin-jks /usr/share/elasticsearch/config/admin.jks
+
+sleep 30
+# for some reason this app will only accept the file type as JKS if it ends with .jks
+/usr/share/elasticsearch/plugins/search-guard-2/tools/sgadmin.sh  \
+   -cd ${HOME}/sgconfig/ \
+   -ks /usr/share/elasticsearch/config/admin.jks \
+   -ts /usr/share/elasticsearch/config/${CLUSTER_NAME}.truststore.jks  \
+	 -cn ${CLUSTER_NAME} \
+	 -kspass kspass \
+	 -tspass tspass \
+   -nhnv
+
+sleep inf
